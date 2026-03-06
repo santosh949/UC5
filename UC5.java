9c4331a66160e21cc75a1626c00d18dbeb34dc6f
Santhosh <vsaisanthosh02@gmail.com>
2026-03-06 22:06:17 +0530

    UC5: Stack-Based Palindrome Checker

--- UC5.java
+++ UC5.java
@@ -1,1 +1,24 @@
+import java.util.Stack;
 
+public class UseCase5PalindromeCheckerApp {
+    public static void main(String[] args) {
+        String word = "madam";
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < word.length(); i++) {
+            stack.push(word.charAt(i));
+        }
+
+        String reversed = "";
+
+        while (!stack.isEmpty()) {
+            reversed = reversed + stack.pop();
+        }
+
+        if (word.equals(reversed)) {
+            System.out.println(word + " is a Palindrome");
+        } else {
+            System.out.println(word + " is not a Palindrome");
+        }
+    }
+}
